c3fb286f9d66908afbe9cc1daabf71a38dfa26ab
masakiyamada <masaki.yamada@yamada-lab.co.jp>
2026-03-23 00:05:20 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error(
+    "Root element #root not found. Make sure there is an element with id='root' in your index.html.",
+  );
+}
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
